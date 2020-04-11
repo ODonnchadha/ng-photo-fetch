c5e2ad70350392ff819d21ba4f68ca800a01d00f
ODonnchadha <danieldonahue1231@hotmail.com>
2020-04-11 08:29:45 -0500

    Initial component and service. Addition of HttpClientModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { FetchPhotoComponent } from './components/fetch-photo-component/fetch-photo-component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FetchPhotoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/fetch-photo-component/fetch-photo-component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fetch-photo-component',
+  templateUrl: './fetch-photo-component.html',
+  styleUrls: ['./fetch-photo-component.css']
+})
+export class FetchPhotoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
